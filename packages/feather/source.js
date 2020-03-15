e79591269f892ff5e6c1231311fcf0223848e353
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-03-15 14:30:24 +0000

    Rename package icon to package-icon

--- packages/feather/source.js
+++ packages/feather/source.js
@@ -3,8 +3,9 @@ const feather = require('feather-icons')
 module.exports = async () =>
   Object.keys(feather.icons).map(originalName => {
     const icon = feather.icons[originalName]
+    const cleanedName = originalName === 'package' ? 'package-icon' : originalName
     return {
-      originalName,
+      originalName: cleanedName,
       source: icon.toSvg(),
       pack: 'feather',
       width: icon.attrs.width,
