a82dd2db5008378946fee673e19b405428c5bae2
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-06-30 21:52:46 +0100

    Pick highest resolution of material icon

--- packages/material-filled/source.js
+++ packages/material-filled/source.js
@@ -4,16 +4,27 @@ const path = require('path')
 
 module.exports = async () => {
   const baseDir = path.dirname(require.resolve('material-design-icons-updated'))
-  const sourceFiles = await glob('icons/filled/*/*24px.svg', {cwd: baseDir, absolute: true})
+  const sourceFiles = await glob('icons/filled/*/*.svg', {cwd: baseDir, absolute: true})
 
-  return sourceFiles.map((filename) => {
-    const match = filename.match(/ic_(.*)_(((\d+)x)?[\d]+px)\.svg$/)
+  const allIcons = sourceFiles.map((filename) => {
+    const match = filename.match(/ic_(.*)_(([\d]+)px)\.svg$/)
     return {
       originalName: match[1].replace(/_/g, '-'),
       source: fs.readFileSync(filename).toString(),
       pack: 'material-filled',
       width: match[4] || '24',
-      height: '24',
+      height: match[4] || '24',
     }
   })
+
+  // Pick highest resolution of icon available
+  const icons = new Map()
+  for (const icon of allIcons) {
+    const existingIcon = icons.get(icon.originalName)
+    if (!existingIcon || parseInt(existingIcon.width, 10) < parseInt(icon.width, 10)) {
+      icons.set(icon.originalName, icon)
+    }
+  }
+
+  return [...icons.values()]
 }
--- packages/material-outlined/source.js
+++ packages/material-outlined/source.js
@@ -6,14 +6,25 @@ module.exports = async () => {
   const baseDir = path.dirname(require.resolve('material-design-icons-updated'))
   const sourceFiles = await glob('icons/outline/*/*.svg', {cwd: baseDir, absolute: true})
 
-  return sourceFiles.map((filename) => {
-    const match = filename.match(/ic_(.*)_(((\d+)x)?[\d]+px)\.svg$/)
+  const allIcons = sourceFiles.map((filename) => {
+    const match = filename.match(/ic_(.*)_(([\d]+)px)\.svg$/)
     return {
       originalName: match[1].replace(/_/g, '-'),
       source: fs.readFileSync(filename).toString(),
       pack: 'material-outlined',
       width: match[4] || '24',
-      height: '24',
+      height: match[4] || '24',
     }
   })
+
+  // Pick highest resolution of icon available
+  const icons = new Map()
+  for (const icon of allIcons) {
+    const existingIcon = icons.get(icon.originalName)
+    if (!existingIcon || parseInt(existingIcon.width, 10) < parseInt(icon.width, 10)) {
+      icons.set(icon.originalName, icon)
+    }
+  }
+
+  return [...icons.values()]
 }
