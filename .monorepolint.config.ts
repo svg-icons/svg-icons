036eb86ee7fc55fde5922fa9f88f39d9944b50b6
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-07-15 08:35:32 +0100

    Remove lerna, add tooling from @styled-icons

---
+++ .monorepolint.config.ts
@@ -0,0 +1,77 @@
+import {Config} from '@monorepolint/core'
+
+const config: Config = {
+  rules: {
+    ':file-contents': [
+      {
+        options: {
+          file: '.gitignore',
+          templateFile: './tools/template.gitignore',
+        },
+        excludePackages: ['@svg-icons/pack-builder'],
+      },
+    ],
+
+    ':consistent-dependencies': true,
+
+    ':package-entry': [
+      {
+        options: {
+          entries: {
+            license: 'MIT',
+            funding: {
+              type: 'GitHub',
+              url: 'https://github.com/sponsors/jacobwgillespie',
+            },
+            publishConfig: {
+              access: 'public',
+            },
+          },
+        },
+      },
+    ],
+
+    ':alphabetical-dependencies': true,
+    ':alphabetical-scripts': true,
+
+    ':package-order': {
+      options: {
+        order: [
+          'private',
+          'name',
+          'version',
+          'license',
+          'description',
+          'author',
+          'contributors',
+          'url',
+          'homepage',
+          'repository',
+          'keywords',
+          'funding',
+          'engines',
+          'bin',
+          'main',
+          'module',
+          'types',
+          'typings',
+          'style',
+          'sideEffects',
+          'workspaces',
+          'husky',
+          'lint-staged',
+          'files',
+          'publishConfig',
+          'scripts',
+          'resolutions',
+          'dependencies',
+          'peerDependencies',
+          'devDependencies',
+          'optionalDependencies',
+        ],
+      },
+    },
+  },
+}
+
+module.exports = config
