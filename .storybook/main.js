ad7bb0cf8344c11f8500add53102bcba7012d57f
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-12-22 02:21:37 +0000

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,4 +3,22 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+
+  webpackFinal: async (config) => {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.type === 'asset/resource') {
+        // Removing the rule for SVGs
+        rule.test = /\.(ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+      }
+      return rule
+    })
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: 'raw-loader',
+    })
+
+    console.log(config.module.rules)
+    return config
+  },
 }
